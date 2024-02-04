43198340c32830cef70853e10929c79efad54339
izuwi <freefrip@gmail.com>
2024-02-04 19:02:36 +0200

    valentine

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { YesPageComponent } from './pages/yes-page/yes-page.component';
+import { ValentPageComponent } from './pages/valent-page/valent-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'Valent', pathMatch: 'full'},
+    {path: 'Valent', component: ValentPageComponent},
+    {path: 'Yes', component: YesPageComponent}
+];
